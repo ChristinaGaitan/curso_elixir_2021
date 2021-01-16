82bd11f30d59d0cc469f3aeaa429837cdb204b9a
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-16 10:30:04 -0600

    Remover variable no necesaria

--- sesion_01/counter.exs
+++ sesion_01/counter.exs
@@ -1,14 +1,13 @@
 # Es convención utilizar el operador pipe al inicio
-# Para correrlo desde la terminal
+# La ultima linea de la funcion es el valor de return
+# Para correrlo desde la terminal:
 # elixir counter.exs
 
 defmodule Counter do
   def count_lines do
-    num_lines = File.read!("words")
+    File.read!("words")
     |> String.split()
     |> Enum.count()
-
-    num_lines
   end
 end
 
