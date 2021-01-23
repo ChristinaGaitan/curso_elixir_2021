f3e34e776c24ee3f63e04599d2d9f2064375ef2d
ChristinaGaitan <christina.gaitan@gmail.com>
2021-01-23 10:47:54 -0600

    Add count_lines function to new module

--- sesion_03/counter/lib/counter.ex
+++ sesion_03/counter/lib/counter.ex
@@ -1,8 +1,11 @@
 defmodule Counter do
+  # Si empiezan con @ son atributos de código (NO son variables)
+  # Son metadatos asociados al modulo
   @moduledoc """
   Documentation for `Counter`.
   """
 
+  # El ejemplo se convirte en una prueba
   @doc """
   Hello world.
 
@@ -15,4 +18,14 @@ defmodule Counter do
   def hello do
     :world
   end
+
+  def count_lines(file_name) do
+    num_lines = File.read!(file_name)
+    |> String.split()
+    |> Enum.count()
+
+    {:ok, num_lines}
+  end
+
+  # Cargar la iex con el modulo: iex -S mix
 end
--- sesion_03/counter/test/counter_test.exs
+++ sesion_03/counter/test/counter_test.exs
@@ -1,7 +1,9 @@
 defmodule CounterTest do
-  use ExUnit.Case
-  doctest Counter
+  use ExUnit.Case # Es el modulo para poder utilizar las pruebas
+  doctest Counter # Especifica que en el modulo Counter hay mas pruebas definidas
 
+  # Se utilizan macros, por eso no se utiliza def para definir esta función
+  # Las macros son como los plugins para el compilador
   test "greets the world" do
     assert Counter.hello() == :world
   end
