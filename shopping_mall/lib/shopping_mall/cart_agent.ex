fb5774ab836a39b7c773745aa47ed7b1d20fbad8
ChristinaGaitan <christina.gaitan@gmail.com>
2021-04-17 09:58:03 -0500

    Create cart agent

---
+++ shopping_mall/lib/shopping_mall/cart_agent.ex
@@ -0,0 +1,17 @@
+defmodule ShoppingMall.CartAgent do
+  use Agent
+
+  def start_link(initial_value) do
+    Agent.start_link(fn -> initial_value end, name: __MODULE__)
+  end
+
+  def value do
+    Agent.get(__MODULE__, & &1)
+  end
+
+  def increment do
+    Agent.update(__MODULE__, fn cart ->
+      ShoppingMall.ShoopingCart.add(cart)
+    end)
+  end
+end
