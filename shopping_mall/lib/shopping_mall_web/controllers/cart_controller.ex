096ceb7e4781c6f8c2a646ad55426ff302b54aef
ChristinaGaitan <christina.gaitan@gmail.com>
2021-04-17 09:59:08 -0500

    Use agent in controller and use result in template

--- shopping_mall/lib/shopping_mall_web/controllers/cart_controller.ex
+++ shopping_mall/lib/shopping_mall_web/controllers/cart_controller.ex
@@ -1,7 +1,9 @@
 defmodule ShoppingMallWeb.CartController do
   use ShoppingMallWeb, :controller
+  alias ShoppingMall.CartAgent
 
   def index(conn, _params) do
-    render(conn, "cart_index.html")
+    cart = CartAgent.value()
+    render(conn, "cart_index.html", cart: cart)
   end
 end
