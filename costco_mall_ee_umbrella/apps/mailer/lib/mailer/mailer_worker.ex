a72a24c42977559e3be074e3f5e5396d7597a4ba
ChristinaGaitan <christina.gaitan@gmail.com>
2021-07-10 10:49:23 -0500

    Create client for server

--- costco_mall_ee_umbrella/apps/mailer/lib/mailer/mailer_worker.ex
+++ costco_mall_ee_umbrella/apps/mailer/lib/mailer/mailer_worker.ex
@@ -1,7 +1,25 @@
 defmodule Mailer.MailerWorker do
   use GenServer
 
-  # Callbacks
+  # Client
+
+  def start_link(default) when is_list(default) do
+    GenServer.start_link(__MODULE__, default, name: __MODULE__)
+  end
+
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
+
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
+
+  def current() do
+    GenServer.call(__MODULE__, :current)
+  end
+
+  # Server / Callbacks
 
   @impl true
   def init(stack) do
@@ -11,12 +29,31 @@ defmodule Mailer.MailerWorker do
   end
 
   @impl true
+  # Parametros
+  # 1. Nombre del callback
+  # 2. PID del proceso que seta haciendo la llamda
+  # 3. El estado actual del proceso
+  def handle_call(:current, _from, state) do
+    IO.inspect(self())
+    Mailer.MailerOne.send()
+    {:reply, state, state}
+  end
+  # Response:
+  # 1. Eleme :reply
+  # 2. Response -> estado actual
+  # 3. Actualizacion del estado
+
+  @impl true
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
 
+  # 1 Nombre del callback
+  # 2 Estado
   @impl true
   def handle_cast({:push, element}, state) do
     {:noreply, [element | state]}
   end
+  # 1 :no reply
+  # 2 Estado con el que queremos actualizar
 end
